e0762908c590551a4b5dbddf9372628d19d4dee7
Zena Jun <zena.jun@gmail.com>
2018-07-29 15:03:48 -0400

    styling for mobile

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,31 @@
 'use strict';
 
+var app = {};
+
+app.slider = function () {
+  var windowWidth = $(window).innerWidth();
+
+  if (windowWidth < 675) {
+    $('.slider').bxSlider({
+      pager: true,
+      auto: true,
+      pause: 5000,
+      controls: false
+    });
+  } else {
+    $('.slider').bxSlider({
+      pager: false,
+      auto: true,
+      pause: 5000,
+      controls: false
+    });
+  }
+};
+
+app.init = function () {
+  app.slider();
+};
+
 $(function () {
-  $('.slider').bxSlider();
+  app.init();
 });
